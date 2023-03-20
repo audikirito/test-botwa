4d0a11e0a32d33ee34930db45a1f2ca45c7501b5
audikirito <93035582+audikirito@users.noreply.github.com>
2023-03-20 22:09:31 +0700

    Add files via upload

---
+++ itsme.js
@@ -0,0 +1,6 @@
+const nama = "Audikirito";
+const asal = "Pekalongan, Jawa Tengah";
+const keahlian = ["JavaScript", "HTML", "C++"];
+const umur = 19;
+const pacar = "Waifu anime";
+console.log(`Perkenalkan, nama saya ${nama}. Saya berasal dari ${asal}. Saya memiliki keahlian di bidang ${keahlian.join(", ")}. Saat ini saya berusia ${umur} tahun dan pacar saya bernama ${pacar}.`);
